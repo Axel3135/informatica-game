ad22639fcb3d891c2276af37452255f3a9437bf9
Axel <99491464+Axel3135@users.noreply.github.com>
2023-02-22 10:28:07 +0100

    Begin code

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-# hier gaat set game komen
+#Libraries
+import pygame
+import numpy as np #Deze is waarschijnlijk wel nodig
+import random
+
+#pygame innit
+pygame.init()
+
+#main loop
+
+running = True
+
+while running:
+    screen = pygame.display.set_mode((600,500))
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            running = False
+
+
+pygame.quit()
+    
+
+
+
+
+
+
+
