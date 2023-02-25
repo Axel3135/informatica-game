af86ee487f1e6392c7e496a5984abe8cffeb928e
Ral-Fireball <99501983+Ral-Fireball@users.noreply.github.com>
2023-02-25 17:11:23 +0100

    set check en active cards
    alle kaarten die momenteel op het bord liggen zijn te zien in active cards met checkForSet kan je zien hoeveel en welke sets er zich in de active cards bevinden.

--- setGame.py
+++ setGame.py
@@ -9,20 +9,28 @@ from random import shuffle
 
 class SetGame:
     def __init__(self):
-        self.fillDeck()
+        self.deck = self.fillDeck()
         self.shuffle()
-        print(self.cards)
-        print(len(self.cards))
+        self.active_cards = list()
+        self.addCards(12)
+
+        print(self.active_cards)
+        print(f"active: {len(self.active_cards)}")
+        print(len(self.deck))
 
     def fillDeck(self):
         amounts = range(3)
         colours = ["red", "green", "purple"]
         patterns = ["empty", "striped", "fill"]
         shapes = ["pill", "wave", "diamond"]
-        self.cards = [SetCard(i, j, k, l) for i in amounts for j in colours for k in patterns for l in shapes]  # maakt een deck met alle 81 verschillende kaarten
+        return [SetCard(i, j, k, l) for i in amounts for j in colours for k in patterns for l in shapes]  # maakt een deck met alle 81 verschillende kaarten
 
     def shuffle(self):
-        shuffle(self.cards)
+        shuffle(self.deck)
+    
+    def addCards(self, amount):
+        self.active_cards.extend([self.deck[i] for i in range(amount)])
+        del self.deck[0:amount]
 
     def isSet(self, card1, card2, card3):
         if ((card1.amount == card2.amount and card2.amount == card3.amount) or (card1.amount != card2.amount and card2.amount != card3.amount and card3.amount != card1.amount)):
@@ -31,10 +39,19 @@ class SetGame:
                     if ((card1.shape == card2.shape and card2.shape == card3.shape) or (card1.shape != card2.shape and card2.shape != card3.shape and card3.shape != card1.shape)):
                         return True  # als alles hetzelfde of verschillend is, is het goed.
         return False
-
-
-
-
+    
+    def checkForSet(self):
+        '''function that checks for sets in the current active cards
+        '''
+        sets = list()
+        for i in self.active_cards[0:-2]:
+            for j in self.active_cards[self.active_cards.index(i)+1:-1]:
+                for k in self.active_cards[self.active_cards.index(j)+1:]:# checkt alle combinaties van kaarten (zonder list indexes, zouden permutaties gecheckt worden.)
+                    # print(self.active_cards.index(i), self.active_cards.index(j), self.active_cards.index(k))
+                    if (not (i==j or j==k or k==i)):
+                        if self.isSet(i, j, k):
+                            sets.append([i, j, k])
+        return sets
 
 class SetCard:
     def __init__(self, amount, colour, pattern, shape):
@@ -51,6 +68,7 @@ class SetCard:
         self.pattern = patterns[pattern]
         self.shape = shapes[shape]
         self.image = f"{amount}_{colour}_{pattern}_{shape}.png"
+        self.properties = [self.amount, self.colour, self.pattern, self.shape]
 
 
 class Player:
@@ -61,4 +79,9 @@ a2 = SetCard(0, "green", "striped", "diamond")
 a3 = SetCard(0, "purple", "fill", "pill")
 print(a1.pattern)
 b = SetGame()
+bsets = b.checkForSet()
+for i in bsets:
+    print(f"\nset: {bsets.index(i)}")
+    for j in i:
+        print(j.properties)
 print(b.isSet(a1, a2, a3))
