b7d45ac93e87c77f573fe0ca1918553c6a192b2d
Axel <99491464+Axel3135@users.noreply.github.com>
2023-03-20 20:33:06 +0100

    Features gefixt

--- main.py
+++ main.py
@@ -19,10 +19,28 @@ start_game = False
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Set!")
 
+SET_LOGO = pygame.image.load('assets/set_logo.png')
+LOGO = pygame.image.load('assets/4_rij_logo.png')
+LOGO2 = pygame.transform.scale(LOGO, (158, 158))
 
+set_rect = SET_LOGO.get_rect()
+logo2_rect = LOGO2.get_rect()
+
+set_rect.center = 250, 400
+logo2_rect.center = 642, 400
+
+
+
+def intro():
+    WIN.fill(WHITE)
+    WIN.blit(SET_LOGO, set_rect)
+    WIN.blit(LOGO2, logo2_rect)
+    
+    
 def main():
     run = True
     clock = pygame.time.Clock()
+    starting = True
 
     while run:
         clock.tick(FPS)
@@ -34,12 +52,20 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = event.pos
                 print(pos)
-                if board.set_box.collidepoint(pos):
+                if set_rect.collidepoint(pos):
                     print("Set! clicked!")
-                elif board.four_box.collidepoint(pos):
+                    starting = False
+                    set_rect.center = -100,-100
+                    logo2_rect.center = -100,-100
+                elif logo2_rect.collidepoint(pos):
                     print("4 in a row clicked!")
-        board.draw_board(WIN)
-        board.start_screen(WIN)
+                    starting = False
+                    set_rect.center = -100,-100
+                    logo2_rect.center = -100,-100
+        if starting == True:
+            intro()
+        else:
+            board.draw_board(WIN)
         
             
         pygame.display.update()
